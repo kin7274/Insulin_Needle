89068c687a8a8e2fd792f65dd4dba935559b07f8
yangjihoon <kin7274@naver.com>
2018-09-22 23:45:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/administrator/insulin_needle/Intro.java
@@ -0,0 +1,41 @@
+package com.example.administrator.insulin_needle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// 앱 시작 전 3초 대기 화면
+public class Intro extends Activity {
+    Handler handler = new Handler();
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            // 3초 뒤에 다음화면(MainActivity)으로 넘어간다
+            Intent intent = new Intent(Intro.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 3초 뒤에 Runnable 객체 수행
+        handler.postDelayed(r, 3000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 예약 취소
+        handler.removeCallbacks(r);
+    }
+}
